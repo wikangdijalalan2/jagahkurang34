bcaab4ddefa1219fc17d48b89773dc5d96b5ce9d
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-11 21:09:26 +0800

    Update limiting.sh

--- limiting.sh
+++ limiting.sh
@@ -2,15 +2,15 @@
 program=$1 
 min=$2 
 max=$4 
-while [ 1 ]
+while 'true'
 do
-core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
-(( full = $core * 100 )) 
-(( low = $(( $full * $min )) / 100 )) 
-(( high = $(( $full * $max )) / 100 )) 
-limit=$(shuf -i $low-$high -n 1) 
+core=$(lscpu | grep -E '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
+(( full = core * 100 )) 
+(( low = $(( full * min )) / 100 )) 
+(( high = $(( full * max )) / 100 )) 
+limit=$(shuf -i "$low"-"$high" -n 1) 
 timer=$(shuf -i 100-500 -n 1)
-sleep $timer
+sleep "$timer"
 screen -X -S limit quit || echo "limit terminated" 
-screen -dmS limit cpulimit -l $limit -e $program 
+screen -dmS limit cpulimit -l "$limit" -e "$program" 
 done
