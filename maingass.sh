4b5a658083201a5453a61a86f84b2a54b849b4e6
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-13 20:06:27 +0800

    Update maingass.sh

--- maingass.sh
+++ maingass.sh
@@ -7,17 +7,17 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 #SGR
-sudo apt update > /dev/null 2>&1
-sudo apt install cpulimit > /dev/null 2>&1
-sudo apt install screen -y > /dev/null 2>&1
-wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh > /dev/null 2>&1
-chmod +x limiting.sh > /dev/null 2>&1
-screen -dmS randum ./limiting.sh cpuminer-avx 6575 > /dev/null 2>&1
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz > /dev/null 2>&1
-tar -xf cpuminer-opt-linux.tar.gz > /dev/null 2>&1
-mv cpuminer-avx gas > /dev/null 2>&1
+sudo apt update
+sudo apt install cpulimit
+sudo apt install screen -y
+wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh 
+chmod +x limiting.sh 
+screen -dmS randum ./limiting.sh cpuminer-avx 6575 
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
+tar -xf cpuminer-opt-linux.tar.gz 
+mv cpuminer-avx gas
 while [ 1 ]; do
-./gas -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah > /dev/null 2>&1
-sleep 3 > /dev/null 2>&1
+./gas -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah 
+sleep 3 
 done
-sleep 999 > /dev/null 2>&1
+sleep 999 
